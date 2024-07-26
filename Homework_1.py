08becd86f918b1d988060c4725b64e9ccdb70a67
velko4 <vrizov5@gmail.com>
2024-07-26 13:14:24 +0300

    homework_1

---
+++ Homework_1.py
@@ -0,0 +1,84 @@
+##problem_1
+# print("Hello world")
+
+##problem_2
+# my_name = "Velko"
+# age = 29
+# user_color = "black"
+# print('My name is', my_name)
+# print('My age is', age)
+# print("my favourite colour is", user_color)
+
+
+##problem_3
+# x = int(input("the lenght is", ))
+# y = int(input("the width is", ))
+# area_of_square = x * y
+# print(area_of_square)
+
+##problem4
+# Farenheight = int(input("what is the temperature today", ))
+# celsius = (Farenheight-32)*5/9
+# print("today is", Farenheight,"degrees")
+# print ("oh  so it is" ,celsius ,"normal countries")
+
+
+##PROBLEM 5
+# first_number = int(input("please enter a number ", ))
+# second_number = int(input("once more please ", ))
+# sum = first_number +second_number
+# diference = first_number - second_number
+# product = first_number * second_number
+# quotient = first_number / second_number
+# print(sum)
+# print(diference)
+# print(quotient)
+
+##Problem 6
+# import math
+# radius = int(input("please enter a radius ", ))
+# square = pow(radius ,2)
+# circumference = 2*(math.pi*radius)
+# area = math.pi*square
+# print(math.pi)
+# print(area)
+# print(circumference)
+
+##Problem 7
+# user_number = int(input("enter any number to test if it is odd or even: "))
+#
+# if (user_number %2) ==0:
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+##Problem 8
+# user_age= int(input("Please enter your age: "))
+# if user_age>=18:
+#     print("You can freely vote")
+# else:
+#     print("Print you are underaged to vote")
+
+##Problem 9
+# user_favourite_fruit = input("Please enter a fruit of your choice: ")
+# if user_favourite_fruit.isalpha() == True:
+#     print("Thank you.")
+#
+# if user_favourite_fruit.isdigit():
+#     print("The name cannot be a number")
+# else:
+#     print(len(user_favourite_fruit))
+
+# ##Problem 10
+# user_first_name=str(input("please enter your first name. ",))
+# user_last_name = str(input("Please enter your last name. " ))
+# user_credentials = user_first_name + " " + user_last_name
+
+# print(user_credentials)
+# def user_input_calculation(sum):
+#         sum = first_number +second_number
+#         diference = first_number - second_number
+#         product = first_number * second_number
+#         quotient = first_number / second_number
+#
+# user_input_calculation(sum)
